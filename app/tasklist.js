d5285c46f7d89abca86b2534e627c4161bc40d71
Brian Mayeshiro <brianmay@hawaii.edu>
2017-02-25 14:53:17 -1000

    Task 2: Tasklist setup

---
+++ app/tasklist.js
@@ -0,0 +1,18 @@
+
+/**Something
+ * Created by brian on 2/25/2017.
+ */
+
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
